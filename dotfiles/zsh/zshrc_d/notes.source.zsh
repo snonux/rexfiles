2aa4db1d731dac99730faba28d4741faf6d9c764
Paul Buetow <paul@buetow.org>
2024-01-10 13:38:51 +0200

    add ,nq

--- dotfiles/zsh/zshrc_d/notes.source.zsh
+++ dotfiles/zsh/zshrc_d/notes.source.zsh
@@ -9,4 +9,14 @@ if [ -e $NOTES_DIR ]; then
 
     alias ,ne=notes::edit
     alias cdnodes="cd $NOTES_DIR"
+
+    notes::quick () {
+        local -r name="$1"; shift
+        local -r file="$NOTES_DIR/$name.md"
+        if [ ! -f "$file" ]; then
+            echo "# $name" >> "$file"
+        fi
+        nvim "$file"
+    }
+    alias ,nq=notes::quick
 fi
