2331f23ab245a44678143f53cf567c71ba62ad1a
Paul Buetow <paul@buetow.org>
2024-04-29 19:03:43 +0300

    add shell ai tools

---
+++ dotfiles/zsh/zshrc_d/ai.source.zsh
@@ -0,0 +1,11 @@
+# AI tools in here need API keys to be configured. That's
+# not done automatically through Rex yet.
+
+# Shell genie
+ai::shellgenie::ask () {
+    shell-genie ask "$*" --explain
+}
+alias sga=ai::shellgenie::ask
+
+# ChatGPT command line tool.
+alias gpt=chatgpt
--- dotfiles/zsh/zshrc_d/betatryout.source.zsh
+++ dotfiles/zsh/zshrc_d/betatryout.source.zsh
@@ -1,8 +1,2 @@
 # This file only contains snippets which i am currently only trying out.
 
-# Shell genie
-ai::shellgenie::ask () {
-    shell-genie ask "$*" --explain
-}
-alias sga=ai::shellgenie::ask
-alias gpt=chatgpt
