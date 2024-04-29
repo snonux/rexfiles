cd621f5fbddb165e724d37204753962765faee67
Paul Buetow <paul@buetow.org>
2024-04-29 18:07:02 +0300

    improve genie

--- dotfiles/zsh/zshrc_d/betatryout.source.zsh
+++ dotfiles/zsh/zshrc_d/betatryout.source.zsh
@@ -1,4 +1,7 @@
 # This file only contains snippets which i am currently only trying out.
 
 # Shell genie
-aliasd sg='shell-genie ask'
+shellgenie::ask () {
+    shell-genie ask "$@" --explain
+}
+alias g=shellgenie::ask
