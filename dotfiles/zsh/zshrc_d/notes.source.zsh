035104904bff97caed641110f6653e5b701d477b
Paul Buetow <paul@buetow.org>
2024-05-22 09:49:02 +0300

    add ,nf

--- dotfiles/zsh/zshrc_d/notes.source.zsh
+++ dotfiles/zsh/zshrc_d/notes.source.zsh
@@ -16,7 +16,20 @@ if [ -e $NOTES_DIR ]; then
         if [ ! -f "$file" ]; then
             echo "# $name" >> "$file"
         fi
-        nvim "$file"
+        $EDITOR "$file"
     }
     alias ,nq=notes::quick
+
+    notes::quick::find () {
+        local -r name="$1"; shift
+        local -r note="$(find $NOTES_DIR/ -iname "*$name*.md" | head -n 1)"
+
+        if [ ! -f "$note" ]; then
+            echo "Could not find any note in $NOTES_DIR"
+            return
+        fi
+
+        $EDITOR $note
+    }
+    alias ,nf=notes::quick::find
 fi
