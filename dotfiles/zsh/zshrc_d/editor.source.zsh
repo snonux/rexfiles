0ea12b389d9969c5b080fc1717a0c0aa6f196799
Paul Buetow <paul@buetow.org>
2024-12-13 22:32:55 +0200

    random theme for helix

--- dotfiles/zsh/zshrc_d/editor.source.zsh
+++ dotfiles/zsh/zshrc_d/editor.source.zsh
@@ -2,10 +2,19 @@ export EDITOR=hx
 export VISUAL=$EDITOR
 export GIT_EDITOR=$EDITOR
 
-editor::nvim::cd () {
-    cd ~/.config/nvim
+editor::helix::random_theme () {
+    local theme_dir=/usr/share/helix/runtime/themes
+
+    if [ ! -d $theme_dir ]; then
+        return
+    fi
+
+    local random_theme=$(ls $theme_dir | grep -v random.toml | grep .toml | sort -R | head -n 1)
+    cp $theme_dir/$random_theme $theme_dir/random.toml
 }
 
+if [ $UNAME = Linux ]; then
+    editor::helix::random_theme
+fi
+
 alias vi=$EDITOR
-alias cdvi=editor::nvim::cd
-alias fvi="$EDITOR \"\$(find . -type f | fzf)\""
