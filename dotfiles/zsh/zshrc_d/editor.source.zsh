e5a870880b736ccfb6ebbc793cd4a98431a42986
Paul Buetow <paul@buetow.org>
2024-12-15 13:46:28 +0200

    random helix theme

--- dotfiles/zsh/zshrc_d/editor.source.zsh
+++ dotfiles/zsh/zshrc_d/editor.source.zsh
@@ -1,20 +1,26 @@
 export EDITOR=hx
 export VISUAL=$EDITOR
 export GIT_EDITOR=$EDITOR
+export HELIX_CONFIG_DIR=$HOME/.config/helix
 
 editor::helix::random_theme () {
-    local theme_dir=/usr/share/helix/runtime/themes
-
+    # May add more theme search paths based on OS. This one is
+    # for Fedora Linux, but there is also MacOS, etc.
+    local -r theme_dir=/usr/share/helix/runtime/themes
     if [ ! -d $theme_dir ]; then
-        return
+        echo "Helix theme dir $theme_dir doesnt exist"
     fi
 
-    local random_theme=$(ls $theme_dir | grep -v random.toml | grep .toml | sort -R | head -n 1)
-    cp $theme_dir/$random_theme $theme_dir/random.toml
+    local -r config_file=$HELIX_CONFIG_DIR/config.toml
+    local -r random_theme="$(basename "$(ls $theme_dir \
+        | grep -v random.toml | grep .toml | sort -R \
+        | head -n 1)" | cut -d. -f1)"
+
+    sed "/^theme =/ { s/.*/theme = \"$random_theme\"/; }" \
+        $config_file > $config_file.tmp && 
+        mv $config_file.tmp $config_file
 }
 
-if [ $UNAME = Linux ]; then
+if [ -f $HELIX_CONFIG_DIR/config.toml ]; then
     editor::helix::random_theme
 fi
-
-alias vi=$EDITOR
