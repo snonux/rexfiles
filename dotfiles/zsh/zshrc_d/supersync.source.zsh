08ce9c125d9fed7c12849e88c87f1009f2f022b1
Paul Buetow <paul@buetow.org>
2025-01-03 21:22:18 +0200

    forgot to add this

---
+++ dotfiles/zsh/zshrc_d/supersync.source.zsh
@@ -0,0 +1,23 @@
+export SUPERSYNC_STAMP_FILE=~/.supersync.last
+
+supersync::is_it_time_to_sync () {
+    local -i max_age=86400
+    local -i now=$(date +%s)
+
+    if [ -f $SUPERSYNC_STAMP_FILE ]; then
+        local -i diff=$(( now - $(cat $SUPERSYNC_STAMP_FILE) ))
+        if [ $diff -lt $max_age ]; then
+            return 0
+        fi
+    fi
+
+    echo 'It is time to run supersync!!!'
+}
+
+supersync::sync () {
+    git::repos::index
+    task::sync
+    gos::run
+    echo $(date +%s)> $SUPERSYNC_STAMP_FILE
+}
+alias supersync=supersync::sync
