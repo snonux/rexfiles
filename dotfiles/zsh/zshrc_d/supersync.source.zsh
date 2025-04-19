b05f242719611a7deabed7fffd42ed33504a8c9b
Paul Buetow <paul@buetow.org>
2025-04-19 23:45:52 +0300

    add ai

--- dotfiles/zsh/zshrc_d/supersync.source.zsh
+++ dotfiles/zsh/zshrc_d/supersync.source.zsh
@@ -1,29 +1,26 @@
-export SUPERSYNC_STAMP_FILE=~/.supersync.last
+set -x SUPERSYNC_STAMP_FILE ~/.supersync.last
 
-supersync::is_it_time_to_sync () {
-    local -i max_age=86400
-    local -i now=$(date +%s)
-
-    if [ -f $SUPERSYNC_STAMP_FILE ]; then
-        local -i diff=$(( now - $(cat $SUPERSYNC_STAMP_FILE) ))
-        if [ $diff -lt $max_age ]; then
+function supersync::is_it_time_to_sync
+    set -l max_age 86400
+    set -l now (date +%s)
+    if test -f $SUPERSYNC_STAMP_FILE
+        set -l diff (math $now - (cat $SUPERSYNC_STAMP_FILE))
+        if test $diff -lt $max_age
             return 0
-        fi
-    fi
-
+        end
+    end
     echo 'It is time to run supersync!!!'
-}
+end
 
-supersync::sync () {
+function supersync::sync
     worktime::sync
     uprecords::sync
     task::sync
-
-    if [ -f $GOS_BIN ]; then
+    if test -f $GOS_BIN
         gos
-    fi
-
-    echo $(date +%s)> $SUPERSYNC_STAMP_FILE.tmp
+    end
+    date +%s > $SUPERSYNC_STAMP_FILE.tmp
     mv $SUPERSYNC_STAMP_FILE.tmp $SUPERSYNC_STAMP_FILE
-}
+end
+
 alias supersync=supersync::sync
