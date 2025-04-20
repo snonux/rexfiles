57e5fb29eeee2ed1ec77689794fabb76543dcd49
Paul Buetow <paul@buetow.org>
2025-04-20 13:12:14 +0300

    fix

---
+++ dotfiles/fish/conf.d/alternatives.fish
@@ -0,0 +1,21 @@
+function alternatives::init
+    if type -q bat
+        alias Cat=/usr/bin/cat
+        alias cat=bat
+    end
+    if type -q see
+        alias ca=see
+    end
+    if type -q bit
+        alias Git=/usr/bin/git
+        alias git=bit
+    end
+    if type -q procs
+        alias p='procs'
+    end
+    if type -q zoxide
+        zoxide init fish | source
+    end
+end
+
+alternatives::init
--- dotfiles/fish/conf.d/editor.fish
+++ dotfiles/fish/conf.d/editor.fish
@@ -5,17 +5,6 @@ set -gx HELIX_CONFIG_DIR $HOME/.config/helix
 set -gx COPILOT_MODEL gpt-4o
 set -gx HANDLER copilot
 
-function editor::helix::theme::get_random
-    echo not yet implemented
-end
-
-function editor::helix::theme::set
-    set -l theme $argv[1]
-    set -l config_file $HELIX_CONFIG_DIR/config.toml
-    # sed "/^theme =/ { s/.*/theme = \"$theme\"/; }" $config_file >$config_file.tmp
-    mv $config_file.tmp $config_file
-end
-
 function editor::helix::open_with_lock
     set -l file $argv[1]
     set -l lock "$file.lock"
@@ -36,10 +25,6 @@ function editor::helix::edit::remote
     scp $local_path $remote_uri; or return 1
     echo "LOCAL_PATH=$local_path; REMOTE_URI=$remote_uri" >~/.hx.remote.source
     hx $local_path
-end
-
-if test -f $HELIX_CONFIG_DIR/config.toml
-    # editor::helix::theme::set (editor::helix::theme::get_random)
 end
 
 abbr -a hxl 'editor::helix::open_with_lock'
