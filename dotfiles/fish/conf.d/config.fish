34fcc08b9f4465f3bdbc7bc86880ce2aa269d039
Paul Buetow <paul@buetow.org>
2025-04-20 13:29:28 +0300

    yay

--- dotfiles/fish/conf.d/config.fish
+++ dotfiles/fish/conf.d/config.fish
@@ -6,3 +6,11 @@ set -U fish_user_paths ~/bin ~/go/bin ~/.cargo/bin $fish_user_paths
 # Git stuff
 abbr -a gl 'git log --pretty=oneline --graph --decorate --all'
 abbr -a gp 'begin; git commit -a; and git pull; and git push; end'
+
+for dir in ~/.config/fish/conf.d.work ~/.config/fish/conf.d.local
+    if test -d $dir
+        for file in $dir/*.fish
+            source $file
+        end
+    end
+end
