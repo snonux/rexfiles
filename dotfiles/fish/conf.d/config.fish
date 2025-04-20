5a059b24d82d259268ee19cb774210254e61e749
Paul Buetow <paul@buetow.org>
2025-04-20 22:52:00 +0300

    add sudo alias

--- dotfiles/fish/conf.d/config.fish
+++ dotfiles/fish/conf.d/config.fish
@@ -3,6 +3,8 @@ fish_vi_key_bindings
 # Add paths to PATH
 set -U fish_user_paths ~/bin ~/go/bin ~/.cargo/bin $fish_user_paths
 
+abbr -a s sudo
+
 # Git stuff
 abbr -a gl 'git log --pretty=oneline --graph --decorate --all'
 abbr -a gp 'begin; git commit -a; and git pull; and git push; end'
