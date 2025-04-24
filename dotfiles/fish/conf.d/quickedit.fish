8b3b5a7105f2ee33df0da62940d95240bd249cfe
Paul Buetow <paul@buetow.org>
2025-04-24 07:03:49 +0300

    jo

--- dotfiles/fish/conf.d/quickedit.fish
+++ dotfiles/fish/conf.d/quickedit.fish
@@ -1,5 +1,30 @@
 set -gx QUICKEDIT_DIR ~/QuickEdit
 
+function quickedit::postaction
+    set -l file_path $argv[1]
+    set -l make_run 0
+
+    if test -f Makefile
+        make
+        set make_run 1
+    end
+
+    # Go to git toplevel dir (if exists)
+    cd (dirname $file_path)
+    set -l git_dir (git rev-parse --show-toplevel 2>/dev/null)
+    if test $status -eq 0
+        cd $git_dir
+    end
+    if not $make_run -eq 1 -a test -f Makefile
+        make
+    end
+    if test -d .git
+        git commit -a -m Update
+        git pull
+        git push
+    end
+end
+
 function quickedit
     set -l prev_dir (pwd)
     set -l grep_pattern .
@@ -12,21 +37,7 @@ function quickedit
     set -l file_path (find -L . -type f -not -path '*/.*' | grep "$grep_pattern" | fzf)
     $EDITOR $file_path
 
-    # Go to git toplevel dir (if exists)
-    cd (dirname $file_path)
-    set -l git_dir (git rev-parse --show-toplevel 2>/dev/null)
-    if test $status -eq 0
-        cd $git_dir
-    end
-    if test -f Makefile
-        make
-    end
-    if test -d .git
-        git commit -a -m Update
-        git pull
-        git push
-    end
-
+    quickedit::postaction $file_path
     cd $prev_dir
 end
 
