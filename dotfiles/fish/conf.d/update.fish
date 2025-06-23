ad6e0547bf72a1e8dd470da1d9875fcaa5317d10
Paul Buetow <paul@buetow.org>
2025-06-24 00:41:28 +0300

    Update

--- dotfiles/fish/conf.d/update.fish
+++ dotfiles/fish/conf.d/update.fish
@@ -2,7 +2,12 @@ function update::tools
     go install github.com/golangci/golangci-lint/v2/cmd/golangci-lint@latest
     go install golang.org/x/tools/cmd/goimports@latest
     go install codeberg.org/snonux/tasksamurai/cmd/tasksamurai@latest
-    go install codeberg.org/snonux/gos/cmd/gos@latest
-    go install codeberg.org/snonux/gitsyncer/cmd/gitsyncer@latest
-    doas npm i -g @anthropic-ai/claude-code 2
+    if [ "$(uname)" = Linux ]
+        then
+        go install codeberg.org/snonux/gos/cmd/gos@latest
+        go install codeberg.org/snonux/gitsyncer/cmd/gitsyncer@latest
+        doas npm i -g @anthropic-ai/claude-code 2
+        dias npm install -g @openai/codex
+        fi
+    end
 end
